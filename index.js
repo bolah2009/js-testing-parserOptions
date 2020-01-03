1664e6b0593aefec4611342eef730c691c781f35
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2020-01-03 14:28:29 +0100

    Add function with and without parens

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const fetchWeatherData = async (info = { location: {}, city: 'Lagos,ng' }) => {
+const fetchWeatherData = async (info = { location: {}, city: "Lagos,ng" }) => {
   const uri = `https://api.openweathermap.org/data/2.5/forecast?${info}`;
   const response = await fetch(uri);
   const data = await response.json();
   const { ok } = response;
   return { data, ok };
 };
+
+export const withArrowParens = (params) => params * 10;
+
+export const withoutArrowParens = params => params * 10;
 
 export default fetchWeatherData;
